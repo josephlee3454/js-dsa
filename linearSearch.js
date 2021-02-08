8491d7837f4c59f8e710fd767a0087be8bcc0444
Joe <joseph.lee3454@gmail.com>
2021-02-08 06:35:40 -0800

    added big o info to linear search

--- linearSearch.js
+++ linearSearch.js
@@ -1,6 +1,11 @@
 
+
+///big-O is best 0(1) worst O(n) average 0(n)
+
+
+
 const linearSearch = (arr=[1,2,3,4,5,], num=4 ) => {
-    for(let i = 0 ; i < arr.length; i++){
+    for(let i = 0 ; i < arr.length; i++){//visists each array position until num is found 
       if (arr[i] ===num){
         return i 
       }
@@ -8,7 +13,7 @@ const linearSearch = (arr=[1,2,3,4,5,], num=4 ) => {
     //     console.log("num not here")
     //   }
     }
-    return -1
+    return -1//if it doesnt find the number then it returns -1
   }
   
   console.log(linearSearch())
