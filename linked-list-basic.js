689daa5b9a5d701ba91b07a61c67c176483d1d53
Joe <joseph.lee3454@gmail.com>
2021-02-09 07:57:16 -0800

    added the pop method

--- linked-list-basic.js
+++ linked-list-basic.js
@@ -1,34 +1,71 @@
 class Node{
-    constructor(val){
-      this.val =val;
-      this.next=null;
+  constructor(val){
+    this.val =val;
+    this.next=null;
+  }
+}
+class SinglyLinkedList{
+  constructor(){
+    this.head=null;
+    this.tail = null;
+    this.length = 0 
+
+  }
+  push(val){
+    let newNode = new Node(val)
+    if (!this.head)
+    {
+      this.head = newNode
+      this.tail = this.head
+    }
+    else{
+      this.tail.next = newNode;
+      this.tail = newNode;
+
+    }
+    this.length++
+    return this
+  }
+  traverse(){
+    let curr = this.head;
+    while(curr){
+      console.log(curr.val)
+      curr = curr.next
     }
   }
-  class SinglyLinkedList{
-    constructor(){
-      this.head=null;
-      this.tail = null;
-      this.length = 0 
-  
-    }
-    push(val){
-      let newNode = new Node(val)
-      if (!this.head)
-      {
-        this.head = newNode
-        this.tail = this.head
+  pop(){
+    
+      if(!this.head) return undefined;
+
+      let current = this.head;
+      let newtail = current;
+      while(current.next){
+        newtail= current
+        current = current.next
       }
-      else{
-        this.tail.next = newNode;
-        this.tail = newNode;
-  
-      }
-      this.length++
-      return this
+      this.tail = newtail
+      this.tail.next = null;
+
+      this.length--
+      return current
+      // console.log(this.tail)
+      // console.log(this.head)
     }
-  }
+
   
-  list = new SinglyLinkedList()
-  console.log(list.push(2))
-  
-  console.log(list.push(7))
+}
+
+list = new SinglyLinkedList()
+console.log(list.push(2))
+console.log(list.push(7))
+console.log(list.push(4))
+console.log(list.push(7))
+console.log(list.push(8))
+console.log(list.push(7))
+
+console.log(list.traverse())
+console.log(list.pop())
+console.log(list.pop())
+console.log(list.pop())
+
+console.log(list.traverse())
